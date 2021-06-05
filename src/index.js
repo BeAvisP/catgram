6e4c3e1597d30fa219d8a0634ed8bb6ce204359d
Bea Avis <bea.avis.p@gmail.com>
2021-06-05 19:40:13 +0200

    Config Apollo and graphql to be accesible inside app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+
 import {App} from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://petgram-server-lilac.vercel.app/graphql',
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    , document.getElementById('app'));
