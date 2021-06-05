6ff13df51a169080b78969a1334ee0c9f491b7d1
Bea Avis <bea.avis.p@gmail.com>
2021-06-05 17:36:45 +0200

    Update PhotoCard with likes in localStorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,24 +1,54 @@
 import React, {Fragment, useEffect, useRef, useState} from 'react';
 import {Article, ImgWrapper, Img, Button} from './styles';
-import {MdFavoriteBorder} from 'react-icons/md';
+import {MdFavoriteBorder, MdFavorite } from 'react-icons/md';
 
 const DEFAULT_IMG = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMG}) => {
   const element = useRef(null);
   const [show, setShow] = useState(false);
+  // Create unique key for localStorage
+  const key = `like-${id}`;
+  // Set the initial state with the localStorage value
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key);
+      return like;
+    } catch (e) {
+      return false;
+    }
+  });
 
   useEffect(function() {
-    // To verify if the element is in the viewport and act as a LazyLoad
-    const observer = new window.IntersectionObserver(function(entries) {
-      const {isIntersecting} = entries[0];
-      if (isIntersecting) {
-        setShow(true);
-        observer.disconnect();
-      }
+    // Dynamic import. Only when the hook is used.
+    Promise.resolve(
+    typeof window.IntersectionObserver !== 'undefined' ?
+      window.IntersectionObserver :
+      import('intersection-observer'),
+    ).then(()=> {
+      // To verify if the element is in the viewport and act as a LazyLoad
+      const observer = new window.IntersectionObserver(function(entries) {
+        const {isIntersecting} = entries[0];
+        if (isIntersecting) {
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+      observer.observe(element.current);
     });
-    observer.observe(element.current);
   }, [element]);
+
+  const Icon = liked? MdFavorite : MdFavoriteBorder;
+
+  // To save likes in local storage so they persist on page refresh.
+  const setLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, value);
+      setLiked(value);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <Article ref={element}>
@@ -29,8 +59,8 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMG}) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button>
-            <MdFavoriteBorder size='32px'/> {likes} likes!
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size='32px'/> {likes} likes!
           </Button>
         </Fragment>
       }
