ac055dd46803ec8ec70a51b17639ebca4cd2e967
Bea Avis <bea.avis.p@gmail.com>
2021-06-05 17:04:17 +0200

    Update PhotoCard with intersection observer as a LazyLoad of imgs

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import {ImgWrapper, Img, Button} from './styles';
+import React, {Fragment, useEffect, useRef, useState} from 'react';
+import {Article, ImgWrapper, Img, Button} from './styles';
 import {MdFavoriteBorder} from 'react-icons/md';
 
 const DEFAULT_IMG = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMG}) => {
+  const element = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(function() {
+    // To verify if the element is in the viewport and act as a LazyLoad
+    const observer = new window.IntersectionObserver(function(entries) {
+      const {isIntersecting} = entries[0];
+      if (isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+    observer.observe(element.current);
+  }, [element]);
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px'/> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={element}>
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px'/> {likes} likes!
+          </Button>
+        </Fragment>
+      }
+    </Article>
   );
 };
