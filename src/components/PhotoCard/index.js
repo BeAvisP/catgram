62c1de7f42f5df0fd3978f7d19efa3ca55a3ada4
Bea Avis <bea.avis.p@gmail.com>
2021-06-05 20:58:51 +0200

    Create FavButton component and update PhotoCard Button with new FavButton

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,10 @@
 import React, {Fragment} from 'react';
-import {Article, ImgWrapper, Img, Button} from './styles';
-import {MdFavoriteBorder, MdFavorite} from 'react-icons/md';
+import {Article, ImgWrapper, Img} from './styles';
+
 import {useLocalStorage} from '../../hooks/useLocalStorage';
 import {useNearScreen} from '../../hooks/useNearScreen';
+
+import {FavButton} from '../FavButton/';
 
 const DEFAULT_IMG = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
@@ -12,7 +14,7 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMG}) => {
   const key = `like-${id}`;
   const [liked, setLiked] = useLocalStorage(key, false);
 
-  const Icon = liked? MdFavorite : MdFavoriteBorder;
+  const handleFavClick = () => setLiked(!liked);
 
   return (
     <Article ref={element}>
@@ -23,9 +25,7 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMG}) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px'/> {likes} likes!
-          </Button>
+          <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
         </Fragment>
       }
     </Article>
